f7a8a8fd25985f05b1f8054bf97bf6603ca88aa0
Lela <swimmer2158@gmail.com>
2015-05-04 09:08:59 -0400

    Added some code....trying to get it to work.......

--- friends.py
+++ friends.py
@@ -1,1 +1,36 @@
+import pygame, math
+
+class friend():
+    def __init__(self, image, speed = [0,0], pos = [0,0]):
+        self.image = pygame.image.load(image)
+		self.rect = self.image.get_rect()
+		self.speedx = speed[0]
+		self.speedy = speed[1]
+		self.speed = [self.speedx, self.speedy]
+		self.place(pos)
+		self.didBounceX = False
+		self.didBounceY = False
+		self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1self.living = True
+
+
+    def collidefriend(self, other):
+		if self != other:
+			if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+				if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+					if (self.radius + other.radius) > self.distance(other.rect.center):
+						if not self.didBounceX:
+							self.speedx = -self.speedx
+							self.didBouncex = True
+						if not self.didBounceY:
+							self.speedy = -self.speedy
+							self.didBounceY = True
+							#print "hit friend"
+							
+	def collidebrowser(self, other):
+		if self != other:
+			if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+				if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+					if (self.radius + other.radius) > self.distance(other.rect.center):
+						self.living = False
+
 
--- game.py
+++ game.py
@@ -1,3 +1,16 @@
-import from friends
-import from player
+import browser from friends
+import friend from player
 
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 800 
+height = 600
+size = width, height
+
+bgColor = r,g,b = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+bgImage = pygame.image.load("rsc/bgorg.jpg")
