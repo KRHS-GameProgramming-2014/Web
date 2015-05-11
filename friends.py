70df1f81d6170c3f6b3091c250d59d051e562a7a
Lela <swimmer2158@gmail.com>
2015-05-11 09:12:54 -0400

    Added Code......

--- friends.py
+++ friends.py
@@ -2,16 +2,14 @@ import pygame, math
 
 class friend():
     def __init__(self, image, speed = [0,0], pos = [0,0]):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.speedx = speed[0]
-        self.speedy = speed[1]
-        self.speed = [self.speedx, self.speedy]
-        self.place(pos)
-        self.didBounceX = False
-        self.didBounceY = False
-        self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1self.living = True
-
+        #if friend == "kik":
+            #self.image = pygame.image.load ""
+            #self.color = color
+            #self.value = 
+        #if friend == "twitter":
+            #self.image == pygame.image.load ""
+            #self.color = color
+            #self.value = 
 
     def collidefriend(self, other):
         if self != other:
@@ -28,9 +26,6 @@ class friend():
                             
     def collidebrowser(self, other):
         if self != other:
-            if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
-                if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
-                    if (self.radius + other.radius) > self.distance(other.rect.center):
-                        self.living = False
-
+            if collidebrowser 
+            
 
--- player.py
+++ player.py
@@ -2,15 +2,7 @@ import pygame, math
 
 class browser():
     def __init__(self, image, speed = [0,0], pos = [0,0]):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.speedx = speed[0]
-        self.speedy = speed[1]
-        self.speed = [self.speedx, self.speedy]
-        self.place(pos)
-        self.didBounceX = False
-        self.didBounceY = False
-        self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1
+     
         self.living = True
         
     def place(self, pos):
