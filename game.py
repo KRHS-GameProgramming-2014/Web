c7e45a5f94b2438d445612155dd70f5c3a3452bc
Lela <swimmer2158@gmail.com>
2015-05-26 09:14:16 -0400

    friends and kill pyc, db

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import pygame, sys, random
-from Friends import Friend
-from Player import Browser
+from friend import Friend
+from player import Browser
 from BackGround import BackGround
 
 pygame.init()
- 
+
 clock = pygame.time.Clock()
 
 width = 800 
@@ -31,30 +31,48 @@ while True:
     player = Browser((200,200))
     friends = Friend ((300,300))
     while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_w or event.key == pygame.K_UP:
-					player.go("up")
-				if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-					player.go("right")
-				if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-					player.go("down")
-				if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-					player.go("left")
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_w or event.key == pygame.K_UP:
-					player.go("stop up")
-				if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-					player.go("stop right")
-				if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-					player.go("stop down")
-				if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-					player.go("stop left")
-			
-		all.update(width, height)
-		
-		dirty = all.draw(screen)
-		pygame.display.update(dirty)
-		pygame.display.flip()
-		clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player.go("up")
+                if event.key == pygame.K_d:
+                    player.go("right")
+                if event.key == pygame.K_s:
+                    player.go("down")
+                if event.key == pygame.K_a:
+                    player.go("left")
+                if event.key == pygame.K_UP:
+                    friends.go("up")
+                if event.key == pygame.K_RIGHT:
+                    friends.go("right")
+                if event.key == pygame.K_DOWN:
+                    friends.go("down")
+                if event.key == pygame.K_LEFT:    
+                    friends.go("left")
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("stop up")
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("stop right")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("stop down")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("stop left")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    friends.go("stop up")
+                if event.key == pygame.K_RIGHT:
+                    friends.go("stop right")
+                if event.key == pygame.K_DOWN:
+                    friends.go("stop down")
+                if event.key == pygame.K_LEFT:
+                    friends.go("stop left")        
+            
+        all.update(width, height)
+        
+        dirty = all.draw(screen)
+        pygame.display.update(dirty)
+        pygame.display.flip()
+        clock.tick(60)
